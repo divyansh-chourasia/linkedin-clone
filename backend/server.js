5641f29135e2fe555e46ed23ffe8eccd8719a9f0
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-02-02 21:48:56 +0530

    user route completed

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 import authRoutes from './routes/auth.route.js'
+import userRoutes from './routes/user.route.js'
 import { connectDB } from "./lib/db.js";
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
 
 
 app.listen(PORT, ()=>{
