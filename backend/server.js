fd4d2a75cb3929cbfe66f0709f90a234937e4177
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-02-02 19:57:56 +0530

    auth route completed

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.use(express.json());
+app.use(cookieParser());
+
+import authRoutes from './routes/auth.route.js'
+import { connectDB } from "./lib/db.js";
+
+app.use("/api/v1/auth", authRoutes);
+
+
 app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on http://localhost:${PORT}`);
+    connectDB();
 });
---
+++ backend/test.mailtrap.js
@@ -0,0 +1,24 @@
+
+const client = new MailtrapClient({
+  token: TOKEN,
+});
+
+const sender = {
+  email: "hello@demomailtrap.com",
+  name: "Mailtrap Test",
+};
+const recipients = [
+  {
+    email: "chourasiadivyansh2099@gmail.com",
+  }
+];
+
+client
+  .send({
+    from: sender,
+    to: recipients,
+    subject: "You are awesome!",
+    text: "Congrats for sending test email with Mailtrap!",
+    category: "Integration Test",
+  })
+  .then(console.log, console.error);
